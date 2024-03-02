94883f5405b2a588a1b3793c3011050c4a6a9170
alexanderimanuel <160576098+alexanderimanuel@users.noreply.github.com>
2024-03-02 11:29:58 +0700

    Add files via upload

---
+++ Tugas2/235150607111007 - Alex/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Haloo, selamat datang di perpustakaan Uhuy.. disini, kamu akan menikmati 3 fitur: ");
+        System.out.println("1. Memilih kategori");
+        System.out.println("2. Memilih Buku");
+        System.out.println("3. Mendapatkan informasi terkait dengan buku yang dipilih");
+        System.out.println("=========================================================");
+        Perpustakaan buku1 = new Perpustakaan();
+        buku1.display();
+    }
+}
