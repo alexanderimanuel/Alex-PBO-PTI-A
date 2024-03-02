93181ad368c002f6a17089fbf1dfb13ab793b597
alexanderimanuel <160576098+alexanderimanuel@users.noreply.github.com>
2024-03-02 11:25:03 +0700

    Delete src directory

--- src/Mahasiswa.java
+++
@@ -1,9 +0,0 @@
-public class Mahasiswa {
-    public static void main(String[] args) {
-        PerhitunganBuku budi = new PerhitunganBuku();
-        budi.isiBuku = 50; //50 lembar = 100 halaman
-        budi.kataPerHari = 100; // 100 kata atau 1/2 halaman 
-        budi.banyakHari();
-    }
-}
-
--- src/PerhitunganBuku.java
+++
@@ -1,9 +0,0 @@
-public class PerhitunganBuku {
-    public int isiBuku;
-    public int kataPerHari;
-    public int banyakHari;
-    public void banyakHari() {
-        banyakHari = isiBuku * 4; // 2 dikali 2
-        System.out.print("Banyak hari yang dibutuhkan untuk membaca 1 buku tersebut adalah "+banyakHari+" hari");
-    }
-}
