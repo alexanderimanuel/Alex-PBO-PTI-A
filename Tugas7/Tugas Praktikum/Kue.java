bd57d99af41e7545c3b6d5d4ccb6ebe21af7fd86
alexanderimanuel <160576098+alexanderimanuel@users.noreply.github.com>
2024-05-06 12:24:39 +0700

    Add files via upload

---
+++ Tugas7/Tugas Praktikum/Kue.java
@@ -0,0 +1,19 @@
+public abstract class Kue {
+    private String nama;
+    private double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public abstract double hitungHarga();
+
+    public String toString() {
+        return "Nama: " + nama + "\nHarga: " + hitungHarga();
+    }
+}
---
+++ Tugas7/Tugas Praktikum/KueJadi.java
@@ -0,0 +1,17 @@
+public class KueJadi extends Kue {
+    private double jumlah;
+
+    public KueJadi(String nama, double harga, double jumlah) {
+        super(nama, harga);
+        this.jumlah = jumlah;
+    }
+
+    @Override
+    public double hitungHarga() {
+        return getHarga() * jumlah * 2;
+    }
+
+    public double getJumlah() {
+        return jumlah;
+    }
+}
